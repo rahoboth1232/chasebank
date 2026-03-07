83c35f96672866a9dae6677501ed32e90e0e2247
Priy Gill <your-email@example.com>
2026-03-07 16:42:26 +0530

    changes in ui

---
+++ frontend/src/api/axiosInstance.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { tokenStore } from "../utils/tokenstore";
+
+
+export const api = axios.create({
+  baseURL: "http://127.0.0.1:8000/",
+});
+
+api.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("accessToken");
+
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
